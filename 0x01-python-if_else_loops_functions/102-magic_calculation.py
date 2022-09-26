723552e1e54a351ad64f12cd46f3d1f516305a99
lunarpunk40 <gg1214493@gmail.com>
2022-09-26 12:14:03 +0000

    update

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3y
+# 102-magic_calculation.p
+
+
+def magic_calculation(a, b, c):
+        """Match bytecode provided by Holberton School."""
+            if a < b:
+                        return (c)
+                            if c > b:
+                                        return (a + b)
+                                            return (a*b - c)
