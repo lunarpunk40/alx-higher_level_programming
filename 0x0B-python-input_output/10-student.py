5bfe41616fcd615769870c36e29fb6d39cf752de
lunarpunk40 <89383928+lunarpunk40@users.noreply.github.com>
2023-02-07 19:05:55 +0300

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -9,36 +9,36 @@ of requested attributes or all if none were requested
 
 
 class Student():
+    """
+    Public Attributes:
+        first_name
+        last_name
+        age
+    Public Methods:
+        to_json: retrieves its dictionary representation
+    """
+    def __init__(self, first_name, last_name, age):
         """
-            Public Attributes:
-                first_name
-                last_name
-                age
-            Public Methods:
-                to_json: retrieves its dictionary representation
-            """
-            def __init__(self, first_name, last_name, age):
-                        """
-                                Initializes student with full name and age
-                                """
-                                self.first_name = first_name
-                                        self.last_name = last_name
-                                                self.age = age
+        Initializes student with full name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-                                                    def to_json(self, attrs=None):
-                                                                """
-                                                                        Returns dictionary description with simple data structure
-                                                                        (list, dictionary, dictionary, string)
-                                                                        for JSON serialization of an object
-                                                                        Return:
-                                                                            Only return dict of attrs given to us
-                                                                            Return entire dict if no attrs given
-                                                                        """
-                                                                        if attrs is None:
-                                                                                        return self.__dict__
-                                                                                                else:
-                                                                                                                dic = {}
-                                                                                                                            for att in attrs:
-                                                                                                                                                if att in self.__dict__.keys():
-                                                                                                                                                                        dic[att] = self.__dict__[att]
-                                                                                                                                                                                    return dic
+    def to_json(self, attrs=None):
+        """
+        Returns dictionary description with simple data structure
+        (list, dictionary, dictionary, string)
+        for JSON serialization of an object
+        Return:
+            Only return dict of attrs given to us
+            Return entire dict if no attrs given
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            dic = {}
+            for att in attrs:
+                if att in self.__dict__.keys():
+                    dic[att] = self.__dict__[att]
+            return dic
