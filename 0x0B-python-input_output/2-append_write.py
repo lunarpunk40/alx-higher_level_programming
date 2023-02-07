ff347e2a30185ce2b28b399d7e5bdb8487de9d46
lunarpunk40 <89383928+lunarpunk40@users.noreply.github.com>
2023-02-07 19:12:15 +0300

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,30 +1,11 @@
 #!/usr/bin/python3
 """
-Module 14-pascal_triangle
-Contains function that returns int lists of pascal triangle of any given size
+Module 4-append_write
+Contains function that appends to text file and returns num chars added
 """
 
 
-def pascal_triangle(n):
-    """
-    Return:
-        empty list [] if n <= 0
-        if n is 7, we should expect:
-            [1]
-            [1, 1]
-            [1, 2, 1]
-            [1, 3, 3, 1]
-            [1, 4, 6, 4, 1]
-            [1, 5, 10, 10, 5, 1]
-            [1, 6, 15, 20, 15, 6, 1]
-    """
-    if n <= 0:
-        return []
-    if n == 1:
-        return [[1]]
-
-    triangle = [[1]]
-    for rows in range(n-1):
-        triangle.append([a+b for a, b
-                         in zip([0] + triangle[-1], triangle[-1] + [0])])
-    return triangle
+def append_write(filename="", text=""):
+    """appends to text file and returns num chars added"""
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return(f.write(text))
